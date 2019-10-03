883c40c574f13305824893c32c43c677164f3d44
lbaral <52364087+lbaral@users.noreply.github.com>
2019-10-03 12:35:59 +1000

    Update CTE RECURSION, SELF JOIN.sql

--- CTE RECURSION, SELF JOIN.sql
+++ CTE RECURSION, SELF JOIN.sql
@@ -18,3 +18,37 @@ as
 	Select EmpCTE. Name as Employee, ISNULL(MngCTE.Name,'Super Boss') as Manager,EmpCTE.Level 
 	from EmployeesCTE as EmpCTE left join EmployeesCTE MngCTE
 	on EmpCTE.ManagerId = MngCTE.EmployeeID
+	
+	-------------------
+	---CTE
+WITH C1 AS 
+(   SELECT YEAR(orderdate) AS orderyear, custid   FROM Sales.Orders 
+)
+
+, C2 AS 
+
+(   SELECT orderyear, COUNT(DISTINCT custid) AS numcusts   FROM C1   GROUP BY orderyear 
+) 
+
+SELECT orderyear, numcusts FROM C2 WHERE numcusts > 70;
+
+--------------
+
+--RECURSIVE CTE
+
+WITH EmpsCTE AS 
+(   
+
+SELECT empid, mgrid, firstname, lastname   
+FROM HR.Employees   
+WHERE empid = 2      
+
+UNION ALL      
+
+SELECT C.empid, C.mgrid, C.firstname, C.lastname   
+FROM EmpsCTE AS P     
+JOIN HR.Employees AS C       
+ON C.mgrid = P.empid 
+) 
+
+SELECT empid, mgrid, firstname, lastname FROM EmpsCTE;
